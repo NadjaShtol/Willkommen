a7e7dd41a1bc9cb7a58f4d31cadde7861756a02e
NadjaShtol <stolln@mail.ru>
2023-07-17 21:39:48 +0300

    Homework 2

---
+++ script.js
@@ -0,0 +1,55 @@
+let a = 10;
+a = 20;
+alert(a);
+
+let releaseYear = prompt("года выпуска первого iPhone");
+alert(releaseYear);
+
+let creatorsName = prompt("имя создателя языка JavaScript");
+alert(creatorsName);
+
+let x = 10;
+let up = 2;
+alert(x + up);
+
+let y = 10;
+alert(y - 2);
+
+let i = 10;
+let increase = 2;
+alert(10 * 2);
+
+let result = 2 ** 5;
+alert(result);
+
+let a = 9;
+let b = 2;
+const result = a % b;
+alert (const result); //НЕ ПОЛУЧАЕТСЯ!
+
+let num = 1;
+num = 1 + 5;
+num = 1 - 3;
+num = 1 * 7;
+num = 1 / 3;
+num = 1 + 1;
+num = 1 - 1;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+const user = {
+    name: "Nadja",
+    age: 18,
+    isAdmin = true,
+}    // НЕ ЗНАЮ, КАКОЕ РЕШЕНИЕ ПРАВИЛЬНО ПРО ОБЪЕКТ
+
+let user = prompt("Введите ваше имя?");
+let age = Number( prompt("Сколько вам лет?"));
+let isAdmin = (Добро пожаловать на сайт ${user} вам ${age} лет?);
+
+let name = prompt("Как вас зовут?");
+alert(`Привет, ${name}"!"`);
+
+
